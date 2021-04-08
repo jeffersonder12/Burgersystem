cdbf54c7f799b50b2742de32a522dd7adb10f3ea
Startklar <jeren.jegatheesswaran@edu.tbz.ch>
2021-04-08 13:41:06 +0200

    ghjkggj

--- src/Burger.java
+++ src/Burger.java
@@ -1,2 +1,27 @@
 public class Burger {
+
+    private double price;
+    private String name;
+
+
+public Burger(double price, String name){
+    this.price = price;
+    this.name = name;
+}
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
