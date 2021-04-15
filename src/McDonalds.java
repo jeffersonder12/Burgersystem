e344a8a753c6313776b62dc823c495d7090a720e
luca <luca.bravo11@gmail.com>
2021-04-15 14:48:30 +0200

    funktioniert noni

--- src/McDonalds.java
+++ src/McDonalds.java
@@ -1,23 +1,24 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class McDonalds {
 
     private ArrayList<Burger> burgers = new ArrayList<>();
     private ArrayList<Drink> drinks = new ArrayList<>();
+    Scanner scan;
 
     public void start(){
         System.out.println("Unsere Burger:");
-       Burger burger = new Burger("Cheesburger", 4.5);
+       Burger burger = new Burger("Cheeseburger", 4.5);
        Burger burger1 = new Burger("DoubleMcBacon", 5.60);
 
        burgers.add(burger);
        burgers.add(burger1);
 
-       for(int i = 0; i < burgers.size(); i++){
        for (Burger b : burgers){
-           System.out.println(i + "\t" + b.getName());
-           System.out.println("\t" + b.getPrice() + "\n");
-       }}
+           System.out.println((burgers.indexOf(b) + 1)+ "\t" + b.getName());
+           System.out.println("\t" + "CHF " + b.getPrice() + "\n");
+       }
 
         System.out.println("Unsere Drinks: ");
        Drink drink = new Drink("Cola", "small", 4.5);
@@ -27,9 +28,16 @@ public class McDonalds {
        drinks.add(drink1);
 
        for (Drink d : drinks){
-           System.out.println("\t" + d.getName());
+           System.out.println((drinks.indexOf(d) + 1) +"\t" + d.getName());
            System.out.println("\t" + d.getSize());
-           System.out.println("\t" + d.getPrice() + "\n");
+           System.out.println("\t" + "CHF " + d.getPrice() + "\n");
        }
+
+        System.out.println("Geben Sie die Zahl des Burgers an: ");
+       int decision = scan.nextInt();
+    }
+
+    public void decision(){
+
     }
 }
